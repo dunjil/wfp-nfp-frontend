85ffb781ea4eedf1a9c2562cf6f153523419c56f
Mac <mac@Macs-MacBook-Pro.local>
2026-05-13 00:40:36 +0100

    WIP

--- next.config.ts
+++ next.config.ts
@@ -4,16 +4,12 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '1337',
-        pathname: '/uploads/**',
+        protocol: 'https',
+        hostname: '**',
       },
       {
         protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '1337',
-        pathname: '/uploads/**',
+        hostname: '**',
       },
       {
         protocol: 'https',
